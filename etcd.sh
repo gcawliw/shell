7a38ff8f1e820447c4b578ac4d5b973467a7f2ad
gcawliw <82490503+gcawliw@users.noreply.github.com>
2022-07-23 22:46:59 +0800

    Update etcd.sh

--- etcd.sh
+++ etcd.sh
@@ -19,17 +19,32 @@ chown -R etcd /db/etcd
 cp /db/etcd/etcd /usr/bin
 cp /db/etcd/etcdctl /usr/bin
 
-read -p "请输入ETCD监听地址:" etcd_host
+###
+read -p "请输入本ETCD节点序号（1-3）:" etcd_node
+read -p "请输入ETCD1节点的IP地址:" etcd1_ip
+read -p "请输入ETCD2节点的IP地址:" etcd2_ip
+read -p "请输入ETCD3节点的IP地址:" etcd3_ip
+###
+if [ ${etcd_node} == 1 ];then
+etcd_host=${etcd1_ip}
+elif [ ${etcd_node} == 2 ];then
+etcd_host=${etcd2_ip}
+elif [ ${etcd_node} == 3 ];then
+etcd_host=${etcd3_ip}
+else
+echo "节点序号错误"
+exit
+fi
 
 cat > /etc/etcd/etcd.yml << EOF
-name: etcd.cluster.node01
+name: etcd.cluster.node0${etcd_node}
 initial-advertise-peer-urls: http://${etcd_host}:2380
 data-dir: /db/etcd/etcd.cluster.node01
 listen-peer-urls: http://${etcd_host}:2380
 listen-client-urls: http://${etcd_host}:2379,http://127.0.0.1:2379
 advertise-client-urls: http://${etcd_host}:2379
 initial-cluster-token: etcd.cluster
-initial-cluster: etcd.cluster.node01=http://${etcd_host}:2380,etcd.cluster.node02=http://${etcd_host}:2380,etcd.cluster.node03=http://${etcd_host}:2380
+initial-cluster: etcd.cluster.node01=http://${etcd1_ip}:2380,etcd.cluster.node02=http://${etcd2_ip}:2380,etcd.cluster.node03=http://${etcd3_ip}:2380
 auto-compaction-retention: '1'
 quota-backend-bytes: 8589934592
 initial-cluster-state: new
